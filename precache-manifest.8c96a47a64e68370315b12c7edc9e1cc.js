444f25504f0140f126aae4457843c91976f5838a
Dmitry <dimatt2013@gmail.com>
2020-07-28 17:06:06 +0500

    Updates

---
+++ precache-manifest.8c96a47a64e68370315b12c7edc9e1cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f3a9d1ce9a30c72a2e4cfd183388c6",
+    "url": "/course-react-burger/index.html"
+  },
+  {
+    "revision": "dc350cb5655299047f74",
+    "url": "/course-react-burger/static/css/main.1888e2bd.chunk.css"
+  },
+  {
+    "revision": "98f7a0f7e5b506ad6c11",
+    "url": "/course-react-burger/static/js/2.5866c4a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/course-react-burger/static/js/2.5866c4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc350cb5655299047f74",
+    "url": "/course-react-burger/static/js/main.29598f76.chunk.js"
+  },
+  {
+    "revision": "346961b5fbe0dec5e28c",
+    "url": "/course-react-burger/static/js/runtime-main.c57002ac.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/course-react-burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
